50748245b20e68d6a32b366ce5147a593f03f870
AndrewCarter04 <68480855+AndrewCarter04@users.noreply.github.com>
2023-05-14 14:24:41 +0000

    Move utilities to seperate file

--- static/scripts/app.js
+++ static/scripts/app.js
@@ -1,69 +1,9 @@
 /**
  * Application of Principles of Programming
  * Assignment Submission
+ *
+ * UTILITY FUNCTIONS ARE STORED IN 'utils.js'
  */
-
-//utility functions - DO NOT EDIT OR DELETE
-function getUniqueKey() {
-    return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
-        (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
-    );
-};
-
-// add the description to priorty number, eg: "1 - Highest"
-function getStringPriority(number) {
-  let priority = number;
-  switch (number) {
-    case "1":
-      priority += " - Highest";
-      break;
-    case "2":
-      priority += " - High";
-      break;
-    case "3":
-      priority += " - Medium";
-      break;
-    case "4":
-      priority += " - Low";
-      break;
-    case "5":
-      priority += " - Lowest";
-      break;
-  } 
-  return priority;
-}
-
-function isEditing() {
-
-  var editing = false;
-  
-  var tableBody = document.getElementById("listTableBody");
-  var tableRecords = tableBody.querySelectorAll("tr");
-
-  for (var tableRecord of tableRecords) {
-    var idEntry = tableRecord.querySelector("td");
-    if (idEntry.innerHTML.startsWith("<input")) {
-      editing = true;
-    }
-  }
-
-  return editing;
-  
-}
-
-function updateLocalChanges(localChanges) {
-  
-  var uploadStatus = document.getElementById("uploadChangesStatus");
-  
-  if (!localChanges) {
-    uploadStatus.className = "uploadChangesSaved";
-    uploadStatus.textContent = "All changes saved.";
-  } else {
-    uploadStatus.className = "uploadChangesNotSaved";
-    uploadStatus.textContent = "Changes not saved!";
-  }
-  
-}
 
 /**
  * Event Handlers - had to call them after DOM loaded, otherwise the console was giving a null error for each element
@@ -85,10 +25,6 @@ document.addEventListener("DOMContentLoaded", function() {
   addEventHandlers(); // only works after DOM loaded, otherwise null error
   getToDoEntries();
 });
-
-/**
- * To-Do List
- */
 
 /*
  *
---
+++ static/scripts/utils.js
@@ -0,0 +1,65 @@
+/*
+ *
+ *  Utility Functions
+ *
+ */
+function getUniqueKey() {
+    return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+        (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+    );
+};
+
+// add the description to priorty number, eg: "1 - Highest"
+function getStringPriority(number) {
+  let priority = number;
+  switch (number) {
+    case "1":
+      priority += " - Highest";
+      break;
+    case "2":
+      priority += " - High";
+      break;
+    case "3":
+      priority += " - Medium";
+      break;
+    case "4":
+      priority += " - Low";
+      break;
+    case "5":
+      priority += " - Lowest";
+      break;
+  } 
+  return priority;
+}
+
+function isEditing() {
+
+  var editing = false;
+  
+  var tableBody = document.getElementById("listTableBody");
+  var tableRecords = tableBody.querySelectorAll("tr");
+
+  for (var tableRecord of tableRecords) {
+    var idEntry = tableRecord.querySelector("td");
+    if (idEntry.innerHTML.startsWith("<input")) {
+      editing = true;
+    }
+  }
+
+  return editing;
+  
+}
+
+function updateLocalChanges(localChanges) {
+  
+  var uploadStatus = document.getElementById("uploadChangesStatus");
+  
+  if (!localChanges) {
+    uploadStatus.className = "uploadChangesSaved";
+    uploadStatus.textContent = "All changes saved.";
+  } else {
+    uploadStatus.className = "uploadChangesNotSaved";
+    uploadStatus.textContent = "Changes not saved!";
+  }
+  
+}
